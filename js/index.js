f717e297333a31349e3d7a42d340af2f87dc9d0e
HongseockKim <68464305+HongseockKim@users.noreply.github.com>
2020-07-19 19:36:36 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,114 @@
+$(document).ready(function(){
+    $('#map_con>.inner>article>ul>li>a.buttons').on('mouseenter', function() {
+			$('#map_con>.inner>article').find('ul>li').removeClass('on');
+			$('#map_con>.inner>article').add($(this).parent()).addClass('on');
+		});
+		$('#map_con>.inner>article>ul>li>a.buttons').on('mouseleave', function() {
+			$('#map_con>.inner>article').add($(this).parent()).removeClass('on');
+		});
+
+$('#Portfolio_con .slider li .non a').on('click', function(e){
+    e.preventDefault();
+    alert('업로드 중에 있습니다. 죄송합니다.');
+});
+	
+	
+	
+	
+	/*image slide*/
+	var slideCount = $('#Portfolio_con .slider li').length;
+	var slideFirst = 1;
+	var slideNow = 0;
+	var slidePrev = 0;
+	var slideNext = 0;
+	var timerId = undefined;
+	var duration = 3000;
+	var timerOn = true;
+	slideShow(slideFirst,'change');
+	$('#Portfolio_con>.slider_wrap>.contrall>a.next').on('click', function(e){
+		e.preventDefault();
+		$(this).addClass('on');
+		slideShow(slideNext,'next');
+	});
+	$('#Portfolio_con>.slider_wrap>.contrall>a.prev').on('click', function(e){
+		e.preventDefault();
+		$(this).addClass('on');
+		slideShow(slidePrev,'prev');
+	});
+	
+	$('#Portfolio_con .slider li').on('mouseenter', function(){
+		clearTimeout(timerId);
+			timerOn =false;
+	});
+	$('#Portfolio_con .slider li').on('mouseleave', function(){
+		timerId = setTimeout(function(){slideShow(slideNext,'next');},duration);
+		timerOn = true;
+	});
+	function slideShow(n,direction){
+		clearTimeout(timerId);
+		if(slideNow === 0 || direction ==='change'){
+			slideMove(n);
+		} else{
+			var slideLeft =0;
+			if(direction ==='next'){
+				slideLeft = -100;
+				$('#Portfolio_con>.slider_wrap>.indi_bar>div.bar').addClass('on');
+			} else if(direction === 'prev'){
+				slideLeft = 100;
+				$('#Portfolio_con>.slider_wrap>.indi_bar>div.bar').addClass('on');
+			} else{
+				slideLeft = 0;
+			}
+			$('#Portfolio_con .slider').css({'transition':'left 0.6s','left': slideLeft+'%'}).one('transitionend',function(){
+				slideMove(n);
+				$('#Portfolio_con>.slider_wrap>.indi_bar>div.bar').removeClass('on');
+			});
+		}
+		if(timerOn === true){
+			timerId = setTimeout(function(){
+				slideShow(slideNext,'next');
+			},duration);
+		}
+	}
+	function slideMove(n){
+		slideNow =n;
+		if(slideNow === slideFirst){
+			slideNext = slideNow+1;
+			slidePrev = slideCount;
+		}else if(slideNow ===slideCount){
+			slideNext = 1;
+			slidePrev = slideNow-1;
+		}else{
+			slidePrev = slideNow - 1;
+			slideNext = slideNow + 1;
+		}
+		$('#Portfolio_con>.slider_wrap>.contrall>a').removeClass('on');
+		$('#Portfolio_con .slider').css({'transition':'none','left':0});
+		$('#Portfolio_con .slider li').css({'left':0 ,'display':'none'});
+		$('#Portfolio_con>.slider_wrap>.text_slider_con>.text_slider>li').css({'transition' : 'all 0.6s','opacity':0,'display':'none'});
+		$('#Portfolio_con>.slider_wrap>.text_slider_con>.text_slider>li').eq(slideNow-1).css({'transition':'all 0.6s','display':'block','opacity':1});
+		$('#Portfolio_con>.slider_wrap>.text_slider_con>.text_slider>li').eq(slidePrev-1).css({'transition':'all 0.6s','display':'block','opacity':0});
+		$('#Portfolio_con>.slider_wrap>.text_slider_con>.text_slider>li').eq(slideNext-1).css({'transition':'all 0.6s','display':'block','opacity':0});
+		$('#Portfolio_con .slider li').eq(slidePrev-1).css({'left':-100+'%','display':'block'});
+		$('#Portfolio_con .slider li').eq(slideNow-1).css({'left':0+'%','display':'block'});
+		$('#Portfolio_con .slider li').eq(slideNext-1).css({'left':100+'%','display':'block'});
+		
+		console.log(slidePrev+'/'+slideNow+'/'+slideNext);
+	} 
+	/*scroll Event*/
+    $(window).on('scroll', function() {
+		checkContent();
+	});
+	
+	function checkContent(){
+		var scrollTop = $(document).scrollTop();
+		var minScroll = $('#skill_con .inner .skill_list').offset().top - $(window).height()+800;
+		var maxScroll = $('#skill_con .inner .skill_list').offset().top + $('#skill_con .inner .skill_list').outerHeight(true)-100;
+		if(scrollTop >= minScroll){
+			$('.contents_wrap #skill_con >.inner .skill_list>ul>li').addClass('on');
+		}
+		if(scrollTop >= maxScroll){
+			$('.contents_wrap #skill_con >.inner .skill_list>ul>li').removeClass('on');
+		}
+	}
+});

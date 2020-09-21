e9e3d0ce7f0fbc05ba011c09d6cd6d8923e716d4
HongseockKim <gusduswk610.gmail.com>
2020-09-22 00:49:38 +0900

    09/22 인터렉션 study

--- appleweb/js/index.js
+++ appleweb/js/index.js
@@ -18,7 +18,12 @@
                 messageD: document.querySelector('#scroll_1 .main_message.d')
             },
             values:{
-                messageA_opacity:[0,1]
+                messageA_opacity_in: [0,1, {start: 0.1, end : 0.2}],
+                messageB_opacity_in: [0,1, {start: 0.3, end : 0.4}],
+                messageA_translateY_in:[20, 0,{start:0.1, end: 0.2}],
+
+                messageA_opacity_out: [1,0, {start: 0.25, end : 0.3}],
+                messageA_translateY_out:[0, -20,{start:0.25, end: 0.3}],
             }
         },
         {//1
@@ -64,14 +69,56 @@
                 currentScene = i;
                 break;
             }
-            document.body.setAttribute('id',`show_scen_${currentScene}`);
         }
+        document.body.setAttribute('id',`show_scen_${currentScene}`);
     }
-
+    function calcValues(values,currentYoffset){
+        var rv=null;
+        //현재 씬(스크롤섹션) 에서 스크롤된 범위를 비율로 구하기
+        var scrollHeight = scenInfo[currentScene].scrollHeight;
+        var scrollRatio = currentYoffset / scrollHeight;
+        if(values.length === 3){
+            //start ~ end 사이에 애니메이션 실행
+            var partScrollStart = values[2].start * scrollHeight;
+            var partScrollEnd = values[2].end * scrollHeight;
+            var partScrollHeight = partScrollEnd - partScrollStart;
+            if(currentYoffset >= partScrollStart && currentYoffset <= partScrollEnd){
+                rv = (currentYoffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0];
+            } else if(currentYoffset < partScrollStart) {
+                rv = values[0];
+            }else if(currentYoffset > partScrollEnd){
+                rv = values[1];
+            }
+        } else{
+            rv = scrollRatio * (values[1] - values[0]) + values[0];
+        }
+        return rv;
+    }
     function playAnimation(){
+        var objs = scenInfo[currentScene].objs;
+        var values = scenInfo[currentScene].values;
+        var currentYoffset = yOffset - prevScrolheight;
+        var scrollHeight = scenInfo[currentScene].scrollHeight;
+        var scrollRatio = (yOffset - prevScrolheight) / scrollHeight;
+        // console.log(currentScene,currentYoffset);
         switch (currentScene){
             case 0:
-                console.log('0 play');
+                //console.log('0 play');
+                var messageA_opacity_in = calcValues(values.messageA_opacity_in, currentYoffset);
+                var messageA_opacity_out = calcValues(values.messageA_opacity_out, currentYoffset);
+                var messageA_translateY_in = calcValues(values.messageA_translateY_in, currentYoffset);
+                var messageA_translateY_out = calcValues(values.messageA_translateY_out, currentYoffset);
+                
+                if(scrollRatio <= 0.22) {
+                    //in
+                    objs.messageA.style.opacity = messageA_opacity_in;
+                    objs.messageA.style.transform = `translateY(${messageA_translateY_in}%)`;
+                } else {
+                    //out
+                    objs.messageA.style.opacity = messageA_opacity_out;
+                    objs.messageA.style.transform = `translateY(${messageA_translateY_out}%)`;
+                }
+                console.log(messageA_opacity_in);
                 break;
             case 1:
                 console.log('1 play');
@@ -92,14 +139,18 @@
             prevScrolheight = prevScrolheight + scenInfo[i].scrollHeight;
         }
         if(yOffset > prevScrolheight + scenInfo[currentScene].scrollHeight){
+            enterNewScene = true;
             currentScene++;
+            document.body.setAttribute('id',`show_scen_${currentScene}`);
         }
         if(yOffset < prevScrolheight){
+            enterNewScene= true;
             if(currentScene === 0) return;//브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
             currentScene--;
+            document.body.setAttribute('id',`show_scen_${currentScene}`);
         }
-        document.body.setAttribute('id',`show_scen_${currentScene}`);
-        console.log(currentScene);
+        //console.log(currentScene);
+        if(enterNewScene) return;
         playAnimation();
     }
 

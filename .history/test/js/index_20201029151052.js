eb3b03f373f3e657f34803ff51f6283f8b22e45f
DESKTOP-QRIC9N0\USER <gusduswk610@gamil.com>
2020-10-29 15:11:04 +0900

    update

---
+++ .history/test/js/index_20201029151052.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    var IfHeight = $('.cssnote').offset().top;
+    $(window).on('scroll', function () {
+        var scrollfloor = Math.floor($(window).scrollTop());
+        if (scrollfloor > IfHeight) {
+            $('.cssnote').css({
+                'position': 'fixed',
+                'top': 0
+            });
+        } else {
+            $('.cssnote').css({
+                'position': 'absolute',
+                'top': 50 + 'px'
+            });
+        }
+        docLayout();
+    });
+
+slideShows();
+});
+function docLayout(){
+    var winWidth = $(window).innerWidth();
+    var winHeight = $(window).innerHeight();
+    $('html,body').css({'width':winWidth,'height':winHeight});
+}
+
+
+
+function slideShows(){
+    var noteCount = $('.con').length;
+    var noteNow = 0;
+    var noteNext = 0;
+    var notePrev = 0;
+
+$('.tab li>a').on('click',function(n){
+    n = $(this).parent('li').index();
+    // console.log(n);
+    noteNow = n;
+});
+    console.log(noteNow);
+    slideShow(noteNow);
+    
+    $('.wrapper .con').each(function (i) {
+        $(this).css({
+            'left': (i * 100) + '%'
+        });
+    });
+
+    $('.tab>li>a').on('click', function (e) {
+        e.preventDefault();
+        var idx = $('.tab>li').index($(this).parent());
+        slideShow(noteNow);
+    });
+
+    function slideShow(noteNow) {
+        $('.moving').css({
+            'transition': 'left 0.3s',
+            'left': -((n - 1) * 100) + '%'
+        });
+        $('.tab li').removeClass('on');
+        $('.tab li').eq(n - 1).addClass('on');
+        noteNow = n;
+        noteNext = (n >= noteCount) ? 1 : n + 1;
+        notePrev = (n <= 1) ? noteCount : n - 1;
+    }
+}
--- test/js/index.js
+++ test/js/index.js
@@ -49,7 +49,7 @@ $('.tab li>a').on('click',function(n){
     $('.tab>li>a').on('click', function (e) {
         e.preventDefault();
         var idx = $('.tab>li').index($(this).parent());
-        slideShow(noteNow + 1);
+        slideShow(noteNow);
     });
 
     function slideShow(noteNow) {

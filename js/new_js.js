742cf8a46cc0a2ca11fd475c6b802c0fa0fb8486
Redston Kim <gusduswk610@gmail.com>
2022-10-11 17:57:02 +0900

    update

--- js/new_js.js
+++ js/new_js.js
@@ -37,6 +37,7 @@ function swiperSlide(){
         }
     })
 }
+
 function pageGridlayoutEvent(){
     let gridItem = $('.grid');
     gridItem.imagesLoaded(function(){
@@ -50,14 +51,12 @@ function pageGridlayoutEvent(){
         });
     });
 }
+
 function imageHoverEvent(){
-    let src = null;
     $('.contnet #Portfolio_con .page_con_wrap .page_con .spage_list .grid-item img').on('click',function(){
-        // $('.contentviewr').addClass('open');
        let idx = $(this).parent().index();
        let imagewrapHeight = $('.contentviewr').eq(idx).find('.img_wrap').height() - $('.contentviewr').eq(idx).find('.text_con').height();
-       console.log(idx);
-       console.log(imagewrapHeight);
+
        $(".contentviewr").eq(idx).find('.text_con').css({'height':imagewrapHeight});
        $(".contentviewr").eq(idx).addClass('open');
        $(window).on('resize',function(){
@@ -72,6 +71,7 @@ function imageHoverEvent(){
         $('.contentviewr').find('.text_con').css({'height':'auto'});
     });
 }
+
 function portfolio_list_data(){
     $.ajax({
         url : "../data/data.json",
@@ -81,56 +81,54 @@ function portfolio_list_data(){
             $(data).each(function(v,i){
                 $(i.imagesrc).each(function(e,r){
                     if(r.linkif === "true" && r.subif === "true"){
-                        obj += "<div class='grid-item'>",
-                        obj += "<img src="+r.mainimage+" alt=''>",
-                        obj += "<div class='view'>",
-                        obj += "<a href='"+r.link_1+"' target='_blank'>메인메뉴",
-                        obj += "</a>",
-                        obj += "<a href='"+r.link_2+"' target='_blank'>서브메뉴",
-                        obj += "</a>",
-                        obj += "</div>",
-                        obj += "<div class='skill_image_con mid'>",
-                        obj += "<img src='"+r.skilllink+"' alt=''>",
-                        obj += "<img src='"+r.skilllink_2+"' alt=''>",
-                        obj += "<img src='"+r.skilllink_3+"' alt=''>",
-                        obj += "<img src='"+r.skilllink_4+"' alt=''>",
-                        obj += "<img src='"+r.skilllink_5+"' alt=''>",
-                        obj += "</div>",
-                        obj += "</div>"
+                        obj += "<div class='grid-item'>";
+                        obj += "<img src="+r.mainimage+" alt='image'>";
+                        obj += "<div class='view'>";
+                        obj += "<a href='"+r.link_1+"' target='_blank'>메인메뉴</a>";
+                        obj += "<a href='"+r.link_2+"' target='_blank'>서브메뉴</a>";
+                        obj += "</div>";
+                        obj += "<div class='skill_image_con mid'>";
+                        obj += "<img src='"+r.skilllink+"' alt='image'>";
+                        obj += "<img src='"+r.skilllink_2+"' alt='image'>";
+                        obj += "<img src='"+r.skilllink_3+"' alt='image'>";
+                        obj += "<img src='"+r.skilllink_4+"' alt='image'>";
+                        obj += "<img src='"+r.skilllink_5+"' alt='image'>";
+                        obj += "</div>";
+                        obj += "</div>";
                     }else if(r.linkif === "true" && r.subif === "false"){
-                        obj += "<li class='grid-item'>",
-                        obj += "<img src="+r.mainimage+" alt=''>",
-                        obj += "<div class='view'>",
-                        obj += "<a href='"+r.link_1+"' target='_blank'>메인메뉴",
-                        obj += "</a>",
-                        obj += "</div>",
-                        obj += "<div class='skill_image_con mid'>",
-                        obj += "<img src='"+r.skilllink+"' alt=''>",
-                        obj += "<img src='"+r.skilllink_2+"' alt=''>",
-                        obj += "<img src='"+r.skilllink_3+"' alt=''>",
-                        obj += "<img src='"+r.skilllink_4+"' alt=''>",
-                        obj += "<img src='"+r.skilllink_5+"' alt=''>",
-                        obj += "</div>",
+                        obj += "<li class='grid-item'>";
+                        obj += "<img src="+r.mainimage+" alt='image'>";
+                        obj += "<div class='view'>";
+                        obj += "<a href='"+r.link_1+"' target='_blank'>메인메뉴</a>";
+                        obj += "</div>";
+                        obj += "<div class='skill_image_con mid'>";
+                        obj += "<img src='"+r.skilllink+"' alt='image'>";
+                        obj += "<img src='"+r.skilllink_2+"' alt='image'>";
+                        obj += "<img src='"+r.skilllink_3+"' alt='image'>";
+                        obj += "<img src='"+r.skilllink_4+"' alt='image'>";
+                        obj += "<img src='"+r.skilllink_5+"' alt='image'>";
+                        obj += "</div>";
                         obj += "</li>"
                     }else{
-                        obj += "<li class='grid-item'>",
-                        obj += "<img src="+r.mainimage+" alt=''>",
-                        obj += "<div class='skill_image_con bot'>",
-                        obj += "<img src='"+r.skilllink+"' alt=''>",
-                        obj += "<img src='"+r.skilllink_2+"' alt=''>",
-                        obj += "<img src='"+r.skilllink_3+"' alt=''>",
-                        obj += "<img src='"+r.skilllink_4+"' alt=''>",
-                        obj += "<img src='"+r.skilllink_5+"' alt=''>",
-                        obj += "</div>",
-                        obj += "</li>"
+                        obj += "<li class='grid-item'>";
+                        obj += "<img src="+r.mainimage+" alt='image'>";
+                        obj += "<div class='skill_image_con bot'>";
+                        obj += "<img src='"+r.skilllink+"' alt='image'>";
+                        obj += "<img src='"+r.skilllink_2+"' alt='image'>";
+                        obj += "<img src='"+r.skilllink_3+"' alt='image'>";
+                        obj += "<img src='"+r.skilllink_4+"' alt='image'>";
+                        obj += "<img src='"+r.skilllink_5+"' alt='image'>";
+                        obj += "</div>";
+                        obj += "</li>";
                     }
-                    
-                })
+                });
+
                 $('#portfolio_list li').remove();
                 $('#portfolio_list').append(obj);
                 $('.contnet #Portfolio_con .page_con_wrap .page_con .spage_list .grid-item .skill_image_con img').each(function(){
                     $('img[src = null]').remove();
                 });
+
                 portfolio_content_data();
                 pageGridlayoutEvent();
             })
@@ -145,34 +143,36 @@ function portfolio_content_data(){
             let sobj = "";
             $(data).each(function(i,v){
                 $(v.imagesrc).each(function(si,sv){
-                    sobj += "<div class='contentviewr'>",
-                    sobj += "<div class='clost_btn_wrap'>",
-                    sobj += "<button type='button' class='close_btn'>",
-                    sobj += "<span class='bar bar_1'></span>",
-                    sobj += "<span class='bar bar_2'></span>",
-                    sobj += "</button>",
-                    sobj += "</div>",
-                    sobj += "<div class='content clearfix'>",
-                    sobj += "<div class='img_wrap'>",
-                    sobj += "<img src='"+sv.image_1+"' alt=''>",
-                    sobj += "<img src='"+sv.image_2+"' alt=''>",
-                    sobj += "<img src='"+sv.image_3+"' alt=''>",
-                    sobj += "</div>",
-                    sobj += "<div class='text_con'>",
-                    sobj += "<h3>"+sv.sitename+"</h3>",
-                    sobj += "<div class='scroll_bar'>",
-                    sobj += "<span class='bar'></span>",
-                    sobj += "</div>",
-                    sobj += "</div>",
-                    sobj += "</div>",
-                    sobj += "</div>"
+                    sobj += "<div class='contentviewr'>";
+                    sobj += "<div class='clost_btn_wrap'>";
+                    sobj += "<button type='button' class='close_btn'>";
+                    sobj += "<span class='bar bar_1'></span>";
+                    sobj += "<span class='bar bar_2'></span>";
+                    sobj += "</button>";
+                    sobj += "</div>";
+                    sobj += "<div class='content clearfix'>";
+                    sobj += "<div class='img_wrap'>";
+                    sobj += "<img src='"+sv.image_1+"' alt='image'>";
+                    sobj += "<img src='"+sv.image_2+"' alt='image'>";
+                    sobj += "<img src='"+sv.image_3+"' alt='image'>";
+                    sobj += "</div>";
+                    sobj += "<div class='text_con'>";
+                    sobj += "<h3>"+sv.sitename+"</h3>";
+                    sobj += "<div class='scroll_bar'>";
+                    sobj += "<span class='bar'></span>";
+                    sobj += "</div>";
+                    sobj += "</div>";
+                    sobj += "</div>";
+                    sobj += "</div>";
                 })
             });
+
             $('#modal_wrap div.contentviewr').remove();
             $('#modal_wrap').append(sobj);
             $('#modal_wrap div.contentviewr .img_wrap img').each(function(){
                 $('img[src = undefined]').remove();
-            })
+            });
+
             imageHoverEvent();
         }
 
@@ -182,10 +182,11 @@ function navEvent(){
     $(' .nav_list_wrap .nav_list .nav_item button').on('click',function(){
         let idx = $(this).parent('li').index();
         let pagenames = "";
-        console.log(idx);
+
         $('.nav_list_wrap .nav_list .nav_item button').removeClass('on');
         $(this).addClass('on');
         $('#con_wrtap').children().remove();
+
         if(idx === 0){
             location.reload();
         }else if(idx === 1){
@@ -195,6 +196,7 @@ function navEvent(){
         }else{
             pagenames = $(this).attr('id');
         }
+
         $('#con_wrtap').load(""+pagenames+".html",function(r,s,x){
             if(s = "success"){
                 console.log('성공');
@@ -203,6 +205,7 @@ function navEvent(){
         });
     })
 }
+
 function uiEvent(){
     $('.contnet.ui .ui_con .btn_list .btn_item .sylte_2').on('mouseenter',function(){
         $(this).children('.text').css({'transition':'all 0.3s','width':100 + "%",'opacity':1});
